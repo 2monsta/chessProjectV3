f313b04f6d73fe2edc817a49b6f3589d3065fd8b
Jason Li <Yunf.li37@gmail.com>
2018-01-03 20:09:27 -0500

    king moves

--- src/Game.js
+++ src/Game.js
@@ -190,8 +190,9 @@ export function canMoveKing(toX, toY){
 	const [x, y] = kingPosition;
   const dx = toX - x;
 	const dy = toY - y;
-	// return (Math.abs(dx) === 2 && Math.abs(dy) === 1) || (Math.abs(dx) === 1 && Math.abs(dy) === 2);
-	return (dx, dy);
+	return (Math.abs(dx) === 1 && Math.abs(dy) === 1) ||
+		(Math.abs(dx) === 0 && Math.abs(dy) === 1) ||
+		(Math.abs(dx) === 1 && Math.abs(dy) === 0)
 }
 export function moveKing(toX, toY){
 	kingPosition = [toX, toY];
